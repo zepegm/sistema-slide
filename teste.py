9880fd193c83b00365fd937416ad3d17a2c91f5f
zepegm <zepegm@gmail.com>
2024-02-02 16:57:47 -0300

    update pdf and database

--- teste.py
+++ teste.py
@@ -1,5 +1,15 @@
-import math
+from pptx import Presentation
 
-result = 1/33
+dir = r'C:\Users\giuseppe.manzella\OneDrive - Secretaria da Educação do Estado de São Paulo\IGREJA\Músicas\Escuro\Autor da Minha Fé (UMADECAP).pptx'
 
-print(math.ceil(result))
+file = open(f'{dir}', 'rb')
+prs = Presentation(file)
+
+for slide in prs.slides:
+    for shape in slide.shapes:
+        if not shape.has_text_frame:
+            continue
+        for paragraph in shape.text_frame.paragraphs:
+            for run in paragraph.runs:
+
+                print(run.text)
