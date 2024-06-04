5e4a31135bc1b0d9a497e4586a2247a166af3467
zepegm <59025669+zepegm@users.noreply.github.com>
2024-06-04 07:34:57 -0300

    open second monitor

--- navegador_util.py
+++ navegador_util.py
@@ -4,17 +4,18 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import ElementNotInteractableException
 
-options = webdriver.ChromeOptions()
-options.add_experimental_option("detach", True)
-options.add_experimental_option('useAutomationExtension', False)
-options.add_experimental_option('excludeSwitches', ['enable-automation'])
-#options.add_argument("--start-fullscreen")
-options.add_argument("--kiosk")
+def open_second_chrome():
+    options = webdriver.ChromeOptions()
+    options.add_experimental_option("detach", True)
+    options.add_experimental_option('useAutomationExtension', False)
+    options.add_experimental_option('excludeSwitches', ['enable-automation'])
+    #options.add_argument("--start-fullscreen")
+    options.add_argument("--kiosk")
 
-driver = webdriver.Chrome(options=options)
+    driver = webdriver.Chrome(options=options)
 
 
 
-driver.get("http://localhost:120/slide")
-driver.set_window_position(2000, 0)
-driver.fullscreen_window()
+    driver.get("http://localhost/slide")
+    driver.set_window_position(2000, 0)
+    driver.fullscreen_window()
